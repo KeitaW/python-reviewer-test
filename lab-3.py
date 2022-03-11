0c471014b5fbdae5469deac124cf9b35c323c06d
Keita Watanabe <keitaw09@gmail.com>
2022-03-11 11:18:23 +0900

    fix memory leak

--- lab-3.py
+++ lab-3.py
@@ -13,10 +13,10 @@ def main(argv):
 
 def kickoff_subprocess(cmd, log_file_name):
     process = subprocess.call(cmd, shell=True)
-    file = open(log_file_name, "a+")
-    timestamp = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-    output = timestamp + " Command: "+ cmd[0] + " | Return Code: " + str(process) + "\n"
-    file.write(output)
+    with open(log_file_name, "a+") as file:
+        timestamp = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+        output = timestamp + " Command: "+ cmd[0] + " | Return Code: " + str(process) + "\n"
+        file.write(output)
 
 def upload_output_to_S3(log_file_name):
     f = open(log_file_name, "rb")
